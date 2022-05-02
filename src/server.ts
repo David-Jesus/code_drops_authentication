0e06d4dd23b0333563ac7bb8e4210ef9b519a4c6
David-Jesus <davidjesus357@gmail.com>
2022-05-01 22:28:13 -0300

    tratamento para erros

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
 
 const app= express();
@@ -7,4 +8,15 @@ app.use(express.json());
 
 app.use(router);
 
+app.use(
+    (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if(error.message == "User alread exists!") {
+        return response.status(401).json({
+            status: "Error",
+            message: error.message,
+         });
+    }
+
+    }
+);
 app.listen(3000, () => console.log("Server is running on port 3000"));
